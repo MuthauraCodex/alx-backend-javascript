ece2ce13dbb4e1a06747b484b997e5815977881d
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-06-29 04:07:47 -0700

    0x00-ES6_basic

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,9 @@
-export default function iterateThroughObject (reportWithIterator) {
-  const employees = []
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
 
   for (const employee of reportWithIterator) {
-    employees.push(employee)
+    employees.push(employee);
   }
 
-  return employees.join(' | ')
+  return employees.join(' | ');
 }
