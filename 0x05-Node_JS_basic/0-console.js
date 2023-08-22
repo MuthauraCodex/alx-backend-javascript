0f23ee01aeb52b938fd8cc48030944f43d5444c1
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-08-22 22:57:44 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displayMessage that prints in STDOUT the string argument
+ * @message {message to be printed to stdout}
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
