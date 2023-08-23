e516eb10b5ec4a354cdcfb9c39e6a78a9ff3ebad
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-08-23 00:09:29 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+/**
+ * 1-stdin.js program that will be executed through command line.
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
